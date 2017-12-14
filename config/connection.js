e97e0453170284ce756a6c86d7acc9e18aa94d5a
Morgan Bradley <mbradleystylist@gmail.com>
2017-12-14 02:13:29 +0000

    file structure completed; config, controllers,  models, views and files within created

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+// var inquirer = require("inquirer");
+
+//create connection to bamazondb
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8080,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "burger_db"
+});
+
+// call showItemsAvailable upon connecting
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
