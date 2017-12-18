ab1a64720b9beb7744a128fc2908aa47bc94f57e
Morgan Bradley <mbradleystylist@gmail.com>
2017-12-18 20:59:25 +0000

    set handlebars, express, and body-parser, app listening on port console

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
+var port = 8080;
 var app = express();
 
-app.use(express.static(process.cwd + '/==========filepath========'));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/burgers_controller.js");
+
+// app.use("/", routes);
+
+app.listen(port, function() {
+    console.log("Listening on PORT " + port);
+});
