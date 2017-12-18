a182ff5de8d841a24269a132c7bd0ab9e00cefed
Morgan Bradley <mbradleystylist@gmail.com>
2017-12-18 21:35:55 +0000

    updated index and main.handlebars

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 // // Import routes and give the server access to them.
 // var routes = require("./controllers/burgers_controller.js");
 
-// app.use("/", routes);
+//app.use("/", routes);
 
 app.listen(port, function() {
     console.log("Listening on PORT " + port);
