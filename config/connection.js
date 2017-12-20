d49d2ed71d83123f24c6d7a89117f2889ecf170c
Morgan Bradley <mbradleystylist@gmail.com>
2017-12-20 02:57:37 +0000

    fixed connection, removed port declaration in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
 var mysql = require("mysql");
 // var inquirer = require("inquirer");
 
-//create connection to burger_db
+//create connection to burgers_db
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
     user: "root",
     password: "",
     database: "burgers_db"
@@ -12,8 +11,12 @@ var connection = mysql.createConnection({
 
 // call showItemsAvailable upon connecting
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
     console.log("connected as id " + connection.threadId);
 });
+
 
 module.exports = connection;
